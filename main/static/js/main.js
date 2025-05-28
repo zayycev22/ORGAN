6db530b3d0c7a83125423cf73399afc496d51f26
IndigoShorse <7535415@gmail.com>
2025-05-28 14:43:13 +0300

    Фикс скрипта скролла

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -375,7 +375,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 setTimeout(() => {
                     console.log('info width:', collectionInfo.style.width, 'window:', self.innerWidth)
 
-                    if (container.scrollLeft < self.innerWidth / 6) {
+                    if (container.scrollMarginLeft < container.scrollMarginRight && container.scrollMarginLeft < self.innerWidth / 3) {
                         container.style.width = `calc(50vw + ${container.scrollLeft}px)`;
                         collectionInfo.style.width = `calc(50vw - ${container.scrollLeft}px)`;
                         collectionInfo.style.display = 'flex'
